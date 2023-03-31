bc82b8a1f867435e42e8ed68e92cadb14494a900
VittoriaBasile <vichy.basile@gmail.com>
2023-03-31 16:41:05 +0200

    commit edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+//import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
       <Home />
+      {/* <MyProfile /> */}
       <Footer />
     </div>
   );
